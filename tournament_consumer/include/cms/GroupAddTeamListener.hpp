3a9680ad1b650facdcb174c2fb2cf8b836855d4b
carloszamora08 <carlos10.zamora@gmail.com>
2025-10-30 02:11:06 +0000

    Crear archivos de match

--- tournament_consumer/include/cms/GroupAddTeamListener.hpp
+++ tournament_consumer/include/cms/GroupAddTeamListener.hpp
@@ -5,17 +5,27 @@
 #ifndef LISTENER_GROUPADDTEAM_LISTENER_HPP
 #define LISTENER_GROUPADDTEAM_LISTENER_HPP
 #include "QueueMessageListener.hpp"
+#include "delegate/MatchDelegate.hpp"
+#include "event/TeamAddEvent.hpp"
 
 class GroupAddTeamListener : public QueueMessageListener{
     void processMessage(const std::string& message) override;
+
+    std::shared_ptr<MatchDelegate> matchDelegate;
 public:
     GroupAddTeamListener(const std::shared_ptr<ConnectionManager> &connectionManager);
+    GroupAddTeamListener(const std::shared_ptr<ConnectionManager> &connectionManager, const std::shared_ptr<MatchDelegate> &matchDelegate);
     ~GroupAddTeamListener() override;
 
 };
 
 inline GroupAddTeamListener::GroupAddTeamListener(const std::shared_ptr<ConnectionManager> &connectionManager)
     : QueueMessageListener(connectionManager) {
+}
+
+inline GroupAddTeamListener::GroupAddTeamListener(const std::shared_ptr<ConnectionManager> &connectionManager, const std::shared_ptr<MatchDelegate> &matchDelegate)
+    : QueueMessageListener(connectionManager) {
+        this->matchDelegate = matchDelegate;
 }
 
 inline GroupAddTeamListener::~GroupAddTeamListener() {
@@ -34,6 +44,10 @@ inline void GroupAddTeamListener::processMessage(const std::string &message) {
         
         std::println("Adding team {} to group {} in tournament {}", 
                      teamId, groupId, tournamentId);
+
+        TeamAddEvent teamAddEvent{json["tournamentId"], json["groupId"], json["teamId"]};
+
+        matchDelegate->ProcessTeamAddition(teamAddEvent);
         
     } catch (const std::exception& e) {
         std::println("Error processing message: {}", e.what());
